920b9485e83f8bb6a792d4cdac422d38b71fec14
Artur Muniz <munizart_dev@hotmail.com>
2018-12-21 19:31:15 -0200

    implementing *

--- fdef.js
+++ fdef.js
@@ -6,7 +6,11 @@ function fdef ( namespace ) {
   const checkAll = R.pipe(
       R.zip,
       R.map(
-          R.converge(R.or, [R.tryCatch(R.apply(R.is), R.F), R.apply(R.equals)])
+          R.converge(R.unapply(R.any(R.identity)), [
+            R.tryCatch(R.apply(R.is), R.F),
+            R.apply(R.equals),
+            R.compose(R.equals('*'), R.tryCatch(R.trim, R.always('')), R.head)
+          ])
       ),
       R.reduce(R.and, true),
   )
@@ -17,21 +21,27 @@ function fdef ( namespace ) {
           if (f) {
               return f(...args)
           } else {
-              throw new TypeError(`No definitions for ${namespace}.${name}${args.join(' ')}`)
+              throw new TypeError(`No definitions for ${namespace}.${name} ${args.join(' ')}`)
           }
       } else {
           throw new TypeError(`No definitions for ${namespace}.${name}`)
       }
   }
 
-  return ([name], ...types) => {
+  return ([definition], ...types) => {
+      const [name, asterix] = String(definition).trim().split(' ')
       const funs = funcs[name] || []
+
+      if (!types.length && /^\s*\*\s*$/.test(asterix)) {
+        types = ['*']
+      }
+
       return fn => {
           if (typeof fn === 'function') {
               funcs[name] = [...funs, [types, fn]]
               return apply(name)
           } else {
-              throw new TypeError('You must implement a function for def ` + name + `')
+              throw new TypeError(`You must implement a function for def ${name}`)
           }
       }
   }
--- fdef.spec.js
+++ fdef.spec.js
@@ -1,57 +1,122 @@
-const { always, converge, equals, identity, multiply, unapply } = require('ramda')
-const fc = require('fast-check')
-const fdef = require('./fdef')
+const { always, compose, converge, equals, flip, identity, length, multiply, unapply } = require ('ramda')
+const fc = require ('fast-check')
+const fdef = require ('./fdef')
 
-const eq = unapply(converge(equals))
+const eq = unapply (converge (equals))
+const makeTest = f => v => eq (f, always (v))
 
-describe('fdef', () => {
+describe ('fdef', () => {
 
-    let def = fdef('test')
+    let def = fdef ('test')
 
-    beforeEach(() => {
-        def = fdef('test')
+    beforeEach (() => {
+        def = fdef ('test')
     })
 
-    it('Should returns a function', () => {
-        expect(def `testFn`).toBeInstanceOf(Function)
+    it ('Should returns a function', () => {
+        expect (def `testFn`)
+            .toBeInstanceOf (Function)
     })
 
-    it('Returned function also should return a function', () => {
-        expect(def `id` (identity)).toBeInstanceOf(Function)
+    it ('Returned function also should return a function', () => {
+        expect (def `id *` (identity))
+            .toBeInstanceOf (Function)
     })
 
-    it('Should match anything with *', () => {
+    it ('Should match anything with *', () => {
         const id = def `id *` (identity)
-        fc.assert(fc.property(
-            fc.anything(),
-            eq(identity, id)
+        fc.assert (fc.property (
+            fc.anything (),
+            eq (identity, id)
         ))
 
         const double = def `double *` (x => x+x)
-        fc.assert(fc.property(
-            fc.nat(),
-            eq(multiply(2), double)
+        fc.assert (fc.property (
+            fc.nat (),
+            eq (multiply (2), double)
         ))
 
     })
 
-    it('Should match by type constructor', () => {
+    it ('Should match by type constructor', () => {
         const f =
-            def `f ${String}`  (always('string'))
-            def `f ${Number}`  (always('number'))
-            def `f ${Boolean}` (always('boolean'))
-            def `f *`          (always('otherwise'))
+            def `f ${String}`  (always ('string'))
+            def `f ${Number}`  (always ('number'))
+            def `f ${Boolean}` (always ('boolean'))
+            def `f *`          (always ('otherwise'))
 
-        const test = v => eq(f, always(v))
 
-        fc.assert(fc.property(fc.boolean(), b => f(b) === 'boolean'))
+        const test = makeTest (f)
 
-        fc.assert(fc.property(fc.string(), test('string')))
+        fc.assert (fc.property (fc.boolean (), b => f (b) === 'boolean'))
 
-        fc.assert(fc.property(fc.float(), test('number')))
-        fc.assert(fc.property(fc.integer(), test('number')))
-        fc.assert(fc.property(fc.nat(), test('number')))
+        fc.assert (fc.property (fc.string (), test ('string')))
 
-        fc.assert(fc.property(fc.object(), test('otherwise')))
+        fc.assert (fc.property (fc.float (), test ('number')))
+        fc.assert (fc.property (fc.integer (), test ('number')))
+        fc.assert (fc.property (fc.nat (), test ('number')))
+
+        fc.assert (fc.property (fc.object (), test ('otherwise')))
+    })
+
+    it ('should match any number of params', () => {
+        const countParams = unapply (length)
+
+        const arity =
+            def `arity ${Number}` (always ('number'))
+            def `arity *` (countParams)
+            def `arity * ${Number}` (always ('number'))
+            def `arity * *` (countParams)
+            def `arity * * *` (countParams)
+            def `arity * * * *` (countParams)
+            def `arity * * * * *` (countParams)
+
+        const test = makeTest (arity)
+
+        fc.assert (fc.property (
+            fc.string (),
+            test (1)
+        ))
+
+        fc.assert (fc.property (
+            fc.string (),
+            fc.string (),
+            test (2)
+        ))
+
+        fc.assert (fc.property (
+            fc.string (),
+            fc.string (),
+            fc.string (),
+            test (3)
+        ))
+
+        fc.assert (fc.property (
+            fc.string (),
+            fc.string (),
+            fc.string (),
+            fc.string (),
+            test (4)
+        ))
+
+        fc.assert (fc.property (
+            fc.string (),
+            fc.string (),
+            fc.string (),
+            fc.string (),
+            fc.string (),
+            test (5)
+        ))
+
+        fc.assert (fc.property (
+            fc.nat (),
+            test ('number')
+        ))
+
+        fc.assert (fc.property (
+            fc.anything (),
+            fc.nat (),
+            flip (test ('number'))
+        ))
     })
 })
